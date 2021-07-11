7c48c1fb1cfeeca006a69d276ac53b1be369cb47
Gabriel Mantilla Saltos <gmantill@espol.edu.ec>
2021-07-11 15:34:41 -0500

    Update conexion.go
    Consumir primer Api

--- conexion.go
+++ conexion.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	api_get_information()
+}
+
+func api_get_information() {
+	url := "https://world-geo-data.p.rapidapi.com/countries/EC?language=en%2Cru%2Czh%2Ces%2Car%2Cfr%2Cfa%2Cja%2Cpl%2Cit%2Cpt%2Cde"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("x-rapidapi-key", "e80cbef646mshdc00b2f552d3ab5p128cf5jsnaf676b00aaae")
+	req.Header.Add("x-rapidapi-host", "world-geo-data.p.rapidapi.com")
+	res, _ := http.DefaultClient.Do(req)
+	body, _ := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	fmt.Println(string(body))
 }
