95ac5010b94a105b4718e75dd7e27fcfe8c967d4
Gabriel Mantilla Saltos <gmantill@espol.edu.ec>
2021-07-11 16:29:11 -0500

    Update conexion.go
    Inspeccionar Variables

--- conexion.go
+++ conexion.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
+type nivel_0 struct {
+	Message string
+	Cod     string
+	Count   int
+	List    []nivel_1
+}
+
+type nivel_1 struct {
+	Id    int
+	Name  string
+	Coord coord_
+	Main  main_
+}
+
+type main_ struct {
+	Temp       float32
+	Feels_like float32
+	Temp_min   float32
+	Temp_max   float32
+	Pressure   float32
+	Humidity   float32
+	Sea_level  float32
+	Grnd_level float32
+}
+
+type coord_ struct {
+	Lat  float32
+	Long float32
+}
+
 func main() {
-	api_get_information()
+	//api_get_information()
 	api_get_clima()
 }
 
@@ -23,10 +55,19 @@ func api_get_information() {
 }
 
 func api_get_clima() {
-	url := "https://openweathermap.org/data/2.5/find?q=quito&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric"
+	url := "https://openweathermap.org/data/2.5/find?q=guayaquil&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric"
 	req, _ := http.NewRequest("GET", url, nil)
 	res, _ := http.DefaultClient.Do(req)
 	body, _ := ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
-	fmt.Println(string(body))
+
+	var t nivel_0
+	json.Unmarshal(body, &t)
+	log.Println(t.Message)
+	log.Println(t.Cod)
+	log.Printf("%d", t.Count)
+	log.Println((t.List[0]).Name)
+	log.Println(((t.List[0]).Main).Temp)
+	log.Println(((t.List[0]).Coord).Lat)
+	log.Println(((t.List[0]).Coord).Long)
 }
