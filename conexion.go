857dbac46f926ff17ae33b1251e64a6af2a4bd3d
Gabriel Mantilla Saltos <gmantill@espol.edu.ec>
2021-07-11 15:47:28 -0500

    Update conexion.go
    Consumo de las 2 apis, la primera de informacion por paices y la segunda de datos de clima

--- conexion.go
+++ conexion.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	api_get_information()
+	api_get_clima()
 }
 
 func api_get_information() {
@@ -15,6 +16,15 @@ func api_get_information() {
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("x-rapidapi-key", "e80cbef646mshdc00b2f552d3ab5p128cf5jsnaf676b00aaae")
 	req.Header.Add("x-rapidapi-host", "world-geo-data.p.rapidapi.com")
+	res, _ := http.DefaultClient.Do(req)
+	body, _ := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	fmt.Println(string(body))
+}
+
+func api_get_clima() {
+	url := "https://openweathermap.org/data/2.5/find?q=quito&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric"
+	req, _ := http.NewRequest("GET", url, nil)
 	res, _ := http.DefaultClient.Do(req)
 	body, _ := ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
